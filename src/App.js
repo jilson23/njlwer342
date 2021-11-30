9ebe8f07e2869839aec46fa59e948b80667f8be3
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-30 11:35:18 -0500

    add handlerchange and handlesubmit function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+
+const [task, setTask] = useState([])
+const [value, setValue] = useState('')
+  
+  const handleSubmit = (x) =>{
+    x.preventDefault()
+    setTask(task.concat(value))
+    setValue('')
+  }
+  
+  const handlerChange = (evento) => {
+    setValue(evento.target.value)
+    // if(evento.keyCode === 13){
+    //   console.log(task.concat(evento.target.value))
+    // }
+  }
+
+
+
+  
+  return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
@@ -12,15 +31,18 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {
+              task.map((task, indice) => {
+              return <li key={indice}>{task}</li>
+              })
+            }
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={handleSubmit}>
+             <input type="text" id="new-task" value={value} onChange={handlerChange} placeholder="Ingresa una tarea y oprime Enter" />
            </form>
         </div>
       </div>
     )
-  }
 }
-
 
 export default App;
